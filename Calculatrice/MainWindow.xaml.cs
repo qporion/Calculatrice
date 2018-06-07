1578b0d8595d90489ee83a7e15d722b506583bd3
PORION QUENTIN p1712620 <p1712620@univ-lyon1.fr>
2018-06-07 12:03:07 +0200

    Calcul

--- Calculatrice/MainWindow.xaml.cs
+++ Calculatrice/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -29,7 +30,86 @@ namespace Calculatrice
         {
             var button = sender as Button;
 
-            entryCalcul.Text += button.Content;
+            var strCalcul = entryCalcul.Text + button.Content;
+
+            if(verifyString(strCalcul))
+            {
+                entryCalcul.Text += button.Content;
+            }
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Delete:
+                    entryCalcul.Text = "";
+                    break;
+                case Key.Enter:
+                    entryCalcul.Text = calculStr(entryCalcul.Text).ToString();
+                    break;
+            } 
+        }
+
+        private bool verifyString(String strCalcul) //^(\w |\-)+((\w |\-)+(,|\+|\(|\*|\)))+(\w +)$
+        {
+            foreach (String str in getListWithString(strCalcul))
+            {
+                if (str.Count( c => c == ',') > 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private List<String> getListWithString(String str)
+        {
+            List<String> listStrCalcul = new List<String>();
+            List<char> listPattern = new List<char>();
+            listPattern.Add('-');
+            listPattern.Add('+');
+            listPattern.Add('/');
+            listPattern.Add('*');
+
+            var tmpStr = "";
+            for (var i = 0; i < str.Length; i++)
+            {
+                if (listPattern.Contains(str.ElementAt(i)))
+                {
+                    if (Char.Equals(str.ElementAt(i), '-') && !(Char.IsNumber(str.ElementAtOrDefault(i-1)) || Char.Equals(str.ElementAtOrDefault(i - 1), ')')))
+                    {
+                        tmpStr += str.ElementAt(i);
+                    }
+                    else
+                    {
+                        listStrCalcul.Add(tmpStr);
+                        listStrCalcul.Add("" + str.ElementAt(i));
+                        tmpStr = "";
+                    }
+                }
+                else
+                {
+                    tmpStr += str.ElementAt(i);
+                }
+            }
+
+            listStrCalcul.Add(tmpStr);
+
+            return listStrCalcul;
+        }
+
+        private float calculStr(String str)
+        {
+            List<String> listCalcul = getListWithString(str);
+
+            foreach (String strList in listCalcul)
+            {
+                
+            }
+
+            return 0;
         }
     }
 }
